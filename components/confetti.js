7d17b52c8f452675ef788fd85e81e9cced9d1923
Abdulmalik Ayorinde <55638353+Abdulmalik-Ayorinde@users.noreply.github.com>
2021-08-31 17:38:43 +0100

    Quick fix

--- components/confetti.js
+++ components/confetti.js
@@ -2,7 +2,6 @@ import Confetti from 'react-confetti'
 import  useWindowSize from '@charlietango/use-window-size'
 
 export default function ConfettiComp() {
-  const { width, height } = useWindowSize()
   console.log(width, height)
     return(
              <Confetti width={width} height={height}/>

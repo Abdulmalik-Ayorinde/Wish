323901c7a2ba92f2e0a57cb275c5a6379416dd4e
Abdulmalik Ayorinde <55638353+Abdulmalik-Ayorinde@users.noreply.github.com>
2021-08-31 17:44:59 +0100

    🤦‍♂️🤦‍♂️🤦‍♂️

--- components/confetti.js
+++ components/confetti.js
@@ -2,7 +2,7 @@ import Confetti from 'react-confetti'
 import  useWindowSize from '@charlietango/use-window-size'
 
 export default function ConfettiComp() {
-  console.log(width, height)
+  const { width, height } = useWindowSize()
     return(
              <Confetti width={width} height={height}/>
     )
